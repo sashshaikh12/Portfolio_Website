dd3d72cb429604c3ccf9c9974e4d4340e3340598
sashshaikh12 <sashshaik12@gmail.com>
2025-01-22 19:14:34 +0530

    completed about me with tech skills and made it responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,79 @@
 import React from "react";
 
 function About() {
+
+    const skills = [
+        {
+            name: "C++",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"
+        },
+        {
+            name: "Python",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+        },
+        {
+            name: "JavaScript",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        },
+        {
+            name: "React",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        },
+        {
+            name: "Node.js",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg"
+        },
+        {
+            name: "Express",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg"
+        },
+        {
+            name: "HTML5",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+        },
+        {
+            name: "CSS3",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+        },
+        {
+            name: "Tailwind CSS",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg"
+        },
+        {
+            name: "Git",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+        },
+        {
+            name: "GitHub",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+        },
+        {
+            name: "VS Code",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg"
+        },
+        {
+            name: "Postman",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg"
+        },
+        {
+            name: "C",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"
+        },
+        {
+            name: "SQL",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg"
+        },
+        {
+            name: "golang",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/go/go-original.svg"
+        },
+        {
+            name: "vercel",
+            url: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vercel/vercel-original.svg"
+        },
+
+    ];
+
   return (
     <div className="text-white bg-[#080808] mt-4 font-[Poppins] w-full">
         <h3 className="lg:hidden pt-24 text-3xl font-medium sm:text-[36px] sm:font-[700] md:text-[45px] md:font-[600] pl-10">About Me</h3>
@@ -14,6 +87,20 @@ function About() {
         </div>
         <img src = "CodingPic.jpg" alt = "coding picture" className=" pt-10 lg:pt-48 mx-auto lg:pr-10 h-full object-cover order-first lg:order-none"/>
       </div>
+
+    {/* Tech skills */}
+
+    <h1 className="pt-28 text-[22px] font-[600] text-center mx-auto">Tech Skills</h1>
+    <div className="flex flex-wrap justify-center pt-10 gap-4">
+        {skills.map((skill) => (
+                <div key={skill.name} className="bg-gray-800 p-4 rounded-lg flex flex-col items-center  w-20 sm:w-40 hover:rounded-full hover:bg-white hover:text-black">
+                    <img src={skill.url} alt={skill.name} className="h-8 w-8 sm:h-12 sm:w-12 object-contain" />
+                    <h2 className="mt-4 text-xs sm:text-lg font-medium">{skill.name}</h2>
+                </div>
+                ))}
+    </div>
+        
+
     </div>
   );
 }
