77cced3793b64c3f0bb52c84f427037d4699b592
sashshaikh12 <sashshaik12@gmail.com>
2025-01-26 21:37:26 +0530

    Created responsive footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer(){
     return(
-        <div className="text-white font-[Poppins] w-full">
-            <h1>Footer</h1>
+        <div className="text-white font-[Poppins] w-full bg-[black] flex justify-center h-14 items-center mt-24">
+            <h1 className="text-sm sm:text-base md:text-lg lg:text-xl font-[500]">Created By Hashir Shaikh Suleman</h1>
         </div>
     );
 };
