d53f33a8377d86692f88c54da5ed933f0bcbed2f
sashshaikh12 <sashshaikh12@gmail.com>
2025-01-29 02:11:25 +0530

    added preloading screen

--- src/App.js
+++ src/App.js
@@ -8,19 +8,32 @@ import Education from "./components/Education";
 import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Preloading from "./components/Preloading";
 
 function App() {
+
+  const [preload, setpreload] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setpreload(false);
+    }, 13000);
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Education />
-      <Certifications />
-      <Contact />
-      <Footer />
+      {preload ? <Preloading /> :
+        <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Education />
+        <Certifications />
+        <Contact />
+        <Footer />
+      </div>}
     </div>
   );
 }
