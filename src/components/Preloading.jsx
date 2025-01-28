09b5a22b7fa0995b9cc325f749491b46f16956e1
sashshaikh12 <sashshaikh12@gmail.com>
2025-01-29 02:20:33 +0530

    fixed font size of preloading page and again hopefully final commit :)

--- src/components/Preloading.jsx
+++ src/components/Preloading.jsx
@@ -4,7 +4,7 @@ import { ReactTyped } from "react-typed";
 function Preloading() {
     return(
         <div className="text-white flex justify-center items-center h-screen bg-black">
-            <ReactTyped strings={["Welcome to Hashir's portfolio", "Thank you for visiting!", "Loading your personalized experience..."]} typeSpeed={50} backSpeed={20} className="pt-6 text-base sm:text-3xl  md:pt-5 font-[Poppins] md:text-[44px] md:font-[400]" />
+            <ReactTyped strings={["Welcome to Hashir's portfolio", "Thank you for visiting!", "Loading your personalized experience..."]} typeSpeed={50} backSpeed={20} className="pt-6 text-base sm:text-3xl  md:pt-5 font-[Poppins] md:text-[32px] md:font-[400]" />
         </div>
     );
 };
